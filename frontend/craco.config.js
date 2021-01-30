7da324db8904a9eaff5670f3da8b20e75f13a64d
Aleksandar Dimitrov <git@aleks.bg>
2021-01-30 23:24:39 +0100

    Add auto-recompile for wasm-bindgen

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,31 +1,36 @@
 const path = require('path');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
 
 module.exports = {
   webpack: {
     configure: function override(config, dev) {
-        /**
-        * Add WASM support. SO: https://stackoverflow.com/questions/59319775
-        */
+      /**
+       * Add WASM support. SO: https://stackoverflow.com/questions/59319775
+       */
 
-        // Make file-loader ignore WASM files
-        const wasmExtensionRegExp = /\.wasm$/;
-        config.resolve.extensions.push('.wasm');
-        config.module.rules.forEach(rule => {
-            (rule.oneOf || []).forEach(oneOf => {
-                if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-                    oneOf.exclude.push(wasmExtensionRegExp);
-                }
-            });
+      // Make file-loader ignore WASM files
+      const wasmExtensionRegExp = /\.wasm$/;
+      config.resolve.extensions.push('.wasm');
+      config.module.rules.forEach(rule => {
+        (rule.oneOf || []).forEach(oneOf => {
+          if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
+            oneOf.exclude.push(wasmExtensionRegExp);
+          }
         });
+      });
 
-        // Add a dedicated loader for WASM
-        config.module.rules.push({
-            test: wasmExtensionRegExp,
-            include: path.resolve(__dirname, 'src'),
-            use: [{ loader: require.resolve('wasm-loader'), options: {} }]
-        });
+      // Add a dedicated loader for WASM
+      config.module.rules.push({
+        test: wasmExtensionRegExp,
+        include: path.resolve(__dirname, 'src'),
+        use: [{ loader: require.resolve('wasm-loader'), options: {} }]
+      });
 
-        return config;
+      config.plugins.push(new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "..")
+      }));
+
+      return config;
     }
   }
 }
--- frontend/src/index-cra.tsx
+++ frontend/src/index-cra.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as wasm from "interactive-heatmap";
+import * as wasm from "./pkg";
 
 wasm.greet();
 
