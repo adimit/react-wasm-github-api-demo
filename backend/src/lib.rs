b88d75c522bb91d81400ed0b65ae739b302e9924
Aleksandar Dimitrov <git@aleks.bg>
2021-01-31 13:25:54 +0100

    Add call through the fetch api

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,7 +1,35 @@
+use serde::{Deserialize, Serialize};
 mod utils;
 use cfg_if::cfg_if;
 
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Branch {
+    pub name: String,
+    pub commit: Commit,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Commit {
+    pub sha: String,
+    pub commit: CommitDetails,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CommitDetails {
+    pub author: Signature,
+    pub committer: Signature,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Signature {
+    pub name: String,
+    pub email: String,
+}
 
 cfg_if! {
     if #[cfg(feature = "console_log")] {
@@ -33,4 +61,29 @@ pub fn greet() {
     log::debug!("debug");
     log::error!("error");
     log::warn!("warn");
+}
+
+#[wasm_bindgen]
+pub async fn run(repo: String, branch: String) -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = format!("https://api.github.com/repos/{}/branches/{}", repo, branch);
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    request
+        .headers()
+        .set("Accept", "application/vnd.github.v3+json")?;
+
+    let window = web_sys::window().expect("Should have window");
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let json = JsFuture::from(resp.json()?).await?;
+    let branch_info: Branch = json.into_serde().unwrap();
+
+    Ok(JsValue::from_serde(&branch_info).unwrap())
 }
--- src/index-cra.tsx
+++ src/index-cra.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import * as wasm from "./pkg";
 
 wasm.greet();
+wasm.run("adimit/config", "master").then((result: any) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
